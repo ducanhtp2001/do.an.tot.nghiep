ee7f3b16735390a876f11573ec42043755523ccb
ducanhtp2001 <ducanh123.com@gmail.com>
2024-04-11 20:43:44 +0700

    fix giao dien

--- app/src/main/java/com/example/commyproject/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/commyproject/activities/main/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.commyproject.activities.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
@@ -12,31 +15,39 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.commyproject.R
+import com.example.commyproject.base.BaseActivity
 import com.example.commyproject.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(){
+class MainActivity : BaseActivity(){
     private lateinit var b: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var drawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
-        navController = findNavController(R.id.fragmentContainerView)
-        b.bottomNavigationView.setupWithNavController(navController)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         initView()
         initEvent()
     }
 
-    private fun initEvent() {
+    override fun init() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initObserve() {
+
+    }
+
+    override fun initEvent() {
         b.apply {
             btnMenu.setOnClickListener {
                 drawerNav.visibility = if(drawerNav.visibility == View.VISIBLE) View.GONE else View.VISIBLE
@@ -44,9 +55,55 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
-    private fun initView() {
-        b.apply {
-            drawerNav.visibility = View.GONE
+     override fun initView() {
+
+        navController = findNavController(R.id.fragmentContainerView)
+        b.bottomNavigationView.setupWithNavController(navController)
+        drawerToggle = ActionBarDrawerToggle(this, b.main, R.string.open, R.string.close)
+        b.main.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setSupportActionBar(b.toolbar)
+        b.drawerNav.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.menu_item_profile -> {
+                    Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.menu_item_setting -> {
+                    Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.menu_item_trash -> {
+                    Toast.makeText(this, "trash", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.menu_item_logout -> {
+                    Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@OnNavigationItemSelectedListener true
+        })
+
+//        b.apply {
+//            drawerNav.visibility = View.GONE
+//        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if(b.main.isDrawerOpen(GravityCompat.START)) {
+            b.main.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
--- app/src/main/java/com/example/commyproject/base/BaseActivity.kt
+++ app/src/main/java/com/example/commyproject/base/BaseActivity.kt
@@ -26,7 +26,6 @@ abstract class BaseActivity : AppCompatActivity() {
 
     }
 
-    protected abstract fun getLayoutResId(): Int
     protected abstract fun init()
     protected abstract fun initData()
     protected abstract fun initObserve()
@@ -34,47 +33,12 @@ abstract class BaseActivity : AppCompatActivity() {
     protected abstract fun initEvent()
 
 
-    fun openActivityFirst(context: Context, clazz2: Class<*>, first: Boolean, finish: Boolean) {
+    fun openActivity(context: Context, clazz2: Class<*>, finish: Boolean) {
         val intent = Intent(context, clazz2)
-        intent.putExtra("FIRST_APP", first)
         startActivity(intent)
         if (finish) {
             finish()
         }
-    }
-
-    fun openSplashToMain(context: Context, clazz: Class<*>, data: Any){
-        val intent = Intent(context, clazz)
-        when (data) {
-            is Int -> intent.putExtra("DATA", data)
-            is String -> intent.putExtra("DATA", data)
-            is Boolean -> intent.putExtra("DATA", data)
-        }
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivity(intent)
-        finish()
-    }
-
-    fun openActivityWithData(context: Context, clazz: Class<*>, data: Any, data2: Any) {
-        val intent = Intent(context, clazz)
-        when (data) {
-            is Int -> intent.putExtra("DATA", data)
-            is String -> intent.putExtra("DATA", data)
-            is Boolean -> intent.putExtra("DATA", data)
-        }
-        when (data2) {
-            is Int -> intent.putExtra("DATA2", data2)
-            is String -> intent.putExtra("DATA2", data2)
-            is Boolean -> intent.putExtra("DATA2", data2)
-        }
-        context.startActivity(intent)
-    }
-
-
-    fun openActivityWithDataString(context: Context, clazz2: Class<*>, data: String) {
-        val intent = Intent(context, clazz2)
-        intent.putExtra("data", data)
-        startActivity(intent)
     }
 
     fun openActivityWithObj(context : Context, clazz2: Class<*>, obj : Serializable){
@@ -82,14 +46,4 @@ abstract class BaseActivity : AppCompatActivity() {
         intent.putExtra("OBJ", obj)
         context.startActivity(intent)
     }
-
-    override fun onResume() {
-        super.onResume()
-//        HideNavigation.setStatusBar(R.color.black, this)
-
-    }
-
-
-
-
 }
